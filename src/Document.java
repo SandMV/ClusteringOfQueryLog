191fe49167bab5843a6cd16376bec7525d6eb3fe
SandMV <sandulmv@yandex.ru>
2017-07-30 20:51:15 +0300

    Update structure

--- src/Document.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by sandulmv on 28.07.17.
- */
-public class Document {
-    // URL address
-    String documentName;
-}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by sandulmv on 28.07.17.
- */
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-    }
-}
